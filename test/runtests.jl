50fc7f2b8be6904a62eaa927b1d8e512a2e38752
SimonHansul <simon.hansul@gmail.com>
2025-06-03 19:47:42 +0200

    annotations

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,5 @@ include("DEB/test01_growth_only.jl")
 include("DEB/test02_growth_and_repro.jl")
 
 ##TODO: example using hyperdist
-##TODO: example using different losses
 ##TODO: writing results to disc
 ##TODO: reading results from disc
