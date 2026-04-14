000d3bc47995554bd5e6c741566d87e9456b1af1
Simon Hansul <hansul@gaiac-eco.de>
2026-04-14 12:25:25 +0200

    update FittingProblem constructors

--- src/fitting_problem.jl
+++ src/fitting_problem.jl
@@ -5,35 +5,74 @@ mutable struct FittingProblem
     parameters::Parameters
     completeparams::ComponentVector
     fitted_param_idxs::Vector{Int64}
+end
 
-    """
-    Construt a `FittingProblem`. Needs to be paired with a backend to solve the problem.
 
-    ## Arguments
-
-    - `dataset::Dataset`: The observed data.
-    - `parameters::Parameters`: Specification of which parameters should be fitted, their initial values, etc. 
-    - `simulator::Function`: A function that takes parameters as `ComponentVector` as argument and returns simulations as `Dataset`. We assume that the simulator receivse all parameters as given in `completeparams`, not only the fitted ones.
-    - `completeparams::ComponentVector`: All parameters expected by the simulator. Values not given in `parspec` are fixed. 
-    """
-    function FittingProblem(
+"""
+    FittingProblem(
         dataset::Dataset,
         simulator::Function,
         parameters::Parameters, 
         completeparams::Union{ComponentVector,Nothing} = nothing
         )
 
-        psim = deepcopy(completeparams) # TBD: this is unused; shoud we have prob.psim = psim?
-        fitted_param_idxs = get_fitted_param_idxs(completeparams, parameters) 
+Complete constructor for `FittingProblem`. 
 
-        prob = new()
-        prob.dataset = dataset
-        prob.simulator = simulator
-        prob.parameters = parameters
-        prob.completeparams = isnothing(completeparams) ? deepcopy(parameters.values) : deepcopy(completeparams)
-        prob.fitted_param_idxs = fitted_param_idxs
+## Arguments
 
-        return prob
+- `dataset::Dataset`: The observed data.
+- `parameters::Parameters`: Specification of which parameters should be fitted, their initial values, etc. 
+- `simulator::Function`: A function that takes parameters as `ComponentVector` as argument and returns simulations as `Dataset`. We assume that the simulator receivse all parameters as given in `completeparams`, not only the fitted ones.
+- `completeparams::ComponentVector`: All parameters expected by the simulator. Values not given in `parspec` are fixed. 
 
-    end
+## Examples 
+
+```Julia
+import EcotoxModelFitting as ETMF
+
+prob = ETMF.FittingProblem(data, sim_data, parameters, completeparams)
+sol = ETMF.solve(prob) # uses local optimization by default to solve the fitting problem
+```
+
+"""
+function FittingProblem(
+    dataset::Dataset,
+    simulator::Function,
+    parameters::Parameters, 
+    completeparams::ComponentVector
+    )
+    
+    fitted_param_idxs = get_fitted_param_idxs(completeparams, parameters) 
+
+    prob = FittingProblem(
+        dataset,
+        simulator,
+        parameters,
+        completeparams,
+        fitted_param_idxs
+    )
+    
+    return prob
+
+end
+
+"""
+    FittingProblem(dataset::Dataset, simulator::Function, parameters::Parameters)
+
+Simplified constructor for `FittingProblem`s. 
+This version is preferable for models with few parameters where all parameters needed to run the `simulator` are listed in `parameters`.
+
+`parameters` will internally be translated to a `ComponentVector` that can be accessed from the `simulator`.
+"""
+function FittingProblem(dataset::Dataset, simulator::Function, parameters::Parameters)
+
+    completeparams = to_cvec(parameters)
+
+    return FittingProblem(
+        dataset,
+        simulator, 
+        parameters, 
+        completeparams
+    )
+
 end
